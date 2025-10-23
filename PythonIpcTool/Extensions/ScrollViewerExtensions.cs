c1d4507f748075fe50acf6ac23bc50b3085b0e57
yaochi.wang <yaochi.wang@carux.com>
2025-10-23 16:21:43 +0800

    [Hotfix] ListBox AutoScrollToEndProperty

--- PythonIpcTool/Extensions/ScrollViewerExtensions.cs
+++ PythonIpcTool/Extensions/ScrollViewerExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace PythonIpcTool.Extensions;
 
@@ -11,7 +12,6 @@ namespace PythonIpcTool.Extensions;
 /// </summary>
 public static class ScrollViewerExtensions
 {
-    // 1. Define the Attached Property
     public static readonly DependencyProperty AutoScrollToEndProperty =
         DependencyProperty.RegisterAttached(
             "AutoScrollToEnd",
@@ -19,7 +19,6 @@ public static class ScrollViewerExtensions
             typeof(ScrollViewerExtensions),
             new PropertyMetadata(false, OnAutoScrollToEndChanged));
 
-    // 2. Standard Get/Set methods for the Attached Property
     public static bool GetAutoScrollToEnd(DependencyObject obj)
     {
         return (bool)obj.GetValue(AutoScrollToEndProperty);
@@ -30,50 +29,78 @@ public static class ScrollViewerExtensions
         obj.SetValue(AutoScrollToEndProperty, value);
     }
 
-    // 3. The core logic in the PropertyChanged callback
     private static void OnAutoScrollToEndChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        // We are specifically targeting ItemsControl (like ListView)
         if (d is not ItemsControl itemsControl)
         {
             return;
         }
 
-        // We need to subscribe/unsubscribe to the CollectionChanged event of the ItemsSource.
-        // The ItemsSource must implement INotifyCollectionChanged for this to work (like ObservableCollection).
-        if (itemsControl.ItemsSource is INotifyCollectionChanged collection)
-        {
-            var scrollHandler = new NotifyCollectionChangedEventHandler(
-                (sender, args) =>
+        // The main handler for scrolling when the collection changes
+        var scrollHandler = new NotifyCollectionChangedEventHandler(
+            (sender, args) =>
+            {
+                if (args.Action == NotifyCollectionChangedAction.Add && args.NewItems != null)
                 {
-                    // Only act when new items are added
-                    if (args.Action == NotifyCollectionChangedAction.Add && args.NewItems != null)
-                    {
-                        // --- THIS IS THE FIX ---
-                        // Check if the control is a ListBox or ListView, which have ScrollIntoView.
-                        // The 'is' pattern matching also declares the variable 'listBox'.
-                        if (itemsControl is ListBox listBox)
+                    itemsControl.Dispatcher.BeginInvoke(
+                        new Action(() =>
                         {
-                            object? lastItem = args.NewItems[args.NewItems.Count - 1];
-                            if (lastItem != null)
+                            // --- THIS IS THE TYPE-SAFETY FIX ---
+                            // 1. We must check if the control is a ListBox (or a derived class like ListView).
+                            if (itemsControl is ListBox listBox && listBox.Items.Count > 0)
                             {
+                                // 2. Get the last item.
+                                object lastItem = listBox.Items[listBox.Items.Count - 1];
+
+                                // 3. Now, call ScrollIntoView on the correctly typed 'listBox' variable.
                                 listBox.ScrollIntoView(lastItem);
                             }
-                        }
-                        // You can add a similar check for ListView if needed, but ListBox covers both
-                        // since ListView inherits from ListBox.
-                    }
-                });
+                        }),
+                        DispatcherPriority.ApplicationIdle
+                    );
+                }
+            });
 
-            if ((bool)e.NewValue)
+        // This handler subscribes to events when the control is loaded
+        var loadedHandler = new RoutedEventHandler((sender, args) =>
+        {
+            if (itemsControl.ItemsSource is INotifyCollectionChanged newCollection)
             {
-                // If AutoScrollToEnd is set to true, subscribe to the event.
-                collection.CollectionChanged += scrollHandler;
+                newCollection.CollectionChanged -= scrollHandler; // Ensure no double subscription
+                newCollection.CollectionChanged += scrollHandler;
+
+                // Perform an initial scroll
+                // --- APPLYING THE TYPE-SAFETY FIX HERE AS WELL ---
+                if (itemsControl is ListBox listBox && listBox.Items.Count > 0)
+                {
+                    listBox.ScrollIntoView(listBox.Items[listBox.Items.Count - 1]);
+                }
             }
-            else
+        });
+
+        // This handler cleans up when the control is unloaded
+        var unloadedHandler = new RoutedEventHandler((sender, args) =>
+        {
+            if (itemsControl.ItemsSource is INotifyCollectionChanged newCollection)
             {
-                // If AutoScrollToEnd is set to false, unsubscribe.
-                collection.CollectionChanged -= scrollHandler;
+                newCollection.CollectionChanged -= scrollHandler;
+            }
+        });
+
+        if ((bool)e.NewValue)
+        {
+            // Subscribe to Loaded and Unloaded events
+            itemsControl.Loaded += loadedHandler;
+            itemsControl.Unloaded += unloadedHandler;
+        }
+        else
+        {
+            // Unsubscribe from all events to prevent memory leaks
+            itemsControl.Loaded -= loadedHandler;
+            itemsControl.Unloaded -= unloadedHandler;
+            if (itemsControl.ItemsSource is INotifyCollectionChanged oldCollection)
+            {
+                oldCollection.CollectionChanged -= scrollHandler;
             }
         }
     }
