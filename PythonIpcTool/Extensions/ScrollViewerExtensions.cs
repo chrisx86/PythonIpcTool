e9b378cea45c650e70475fcbd8db8c3c33363865
yaochi.wang <yaochi.wang@carux.com>
2025-10-23 14:43:18 +0800

    [feat] Add OnAutoScrollToEndChanged

---
+++ PythonIpcTool/Extensions/ScrollViewerExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PythonIpcTool.Extensions;
+
+/// <summary>
+/// Provides an attached property to enable robust auto-scrolling for any ItemsControl.
+/// When new items are added, it reliably scrolls to the last item by deferring the scroll action
+/// until after the UI has finished its layout and rendering pass.
+/// </summary>
+public static class ScrollViewerExtensions
+{
+    // 1. Define the Attached Property
+    public static readonly DependencyProperty AutoScrollToEndProperty =
+        DependencyProperty.RegisterAttached(
+            "AutoScrollToEnd",
+            typeof(bool),
+            typeof(ScrollViewerExtensions),
+            new PropertyMetadata(false, OnAutoScrollToEndChanged));
+
+    // 2. Standard Get/Set methods for the Attached Property
+    public static bool GetAutoScrollToEnd(DependencyObject obj)
+    {
+        return (bool)obj.GetValue(AutoScrollToEndProperty);
+    }
+
+    public static void SetAutoScrollToEnd(DependencyObject obj, bool value)
+    {
+        obj.SetValue(AutoScrollToEndProperty, value);
+    }
+
+    // 3. The core logic in the PropertyChanged callback
+    private static void OnAutoScrollToEndChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        // We are specifically targeting ItemsControl (like ListView)
+        if (d is not ItemsControl itemsControl)
+        {
+            return;
+        }
+
+        // We need to subscribe/unsubscribe to the CollectionChanged event of the ItemsSource.
+        // The ItemsSource must implement INotifyCollectionChanged for this to work (like ObservableCollection).
+        if (itemsControl.ItemsSource is INotifyCollectionChanged collection)
+        {
+            var scrollHandler = new NotifyCollectionChangedEventHandler(
+                (sender, args) =>
+                {
+                    // Only act when new items are added
+                    if (args.Action == NotifyCollectionChangedAction.Add && args.NewItems != null)
+                    {
+                        // --- THIS IS THE FIX ---
+                        // Check if the control is a ListBox or ListView, which have ScrollIntoView.
+                        // The 'is' pattern matching also declares the variable 'listBox'.
+                        if (itemsControl is ListBox listBox)
+                        {
+                            object? lastItem = args.NewItems[args.NewItems.Count - 1];
+                            if (lastItem != null)
+                            {
+                                listBox.ScrollIntoView(lastItem);
+                            }
+                        }
+                        // You can add a similar check for ListView if needed, but ListBox covers both
+                        // since ListView inherits from ListBox.
+                    }
+                });
+
+            if ((bool)e.NewValue)
+            {
+                // If AutoScrollToEnd is set to true, subscribe to the event.
+                collection.CollectionChanged += scrollHandler;
+            }
+            else
+            {
+                // If AutoScrollToEnd is set to false, unsubscribe.
+                collection.CollectionChanged -= scrollHandler;
+            }
+        }
+    }
+}
---
+++ PythonIpcTool/Extensions/TextBoxExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PythonIpcTool.Extensions;
+
+/// <summary>
+/// Provides an attached property to enable auto-scrolling to the end for a TextBox.
+/// When the text content changes, it automatically scrolls the view to the last line.
+/// </summary>
+public static class TextBoxExtensions
+{
+    // Define the attached property
+    public static readonly DependencyProperty AutoScrollToEndProperty =
+        DependencyProperty.RegisterAttached(
+            "AutoScrollToEnd",
+            typeof(bool),
+            typeof(TextBoxExtensions),
+            new PropertyMetadata(false, OnAutoScrollToEndChanged));
+
+    // Getter
+    public static bool GetAutoScrollToEnd(DependencyObject obj)
+    {
+        return (bool)obj.GetValue(AutoScrollToEndProperty);
+    }
+
+    // Setter
+    public static void SetAutoScrollToEnd(DependencyObject obj, bool value)
+    {
+        obj.SetValue(AutoScrollToEndProperty, value);
+    }
+
+    /// <summary>
+    /// Callback method executed when the AutoScrollToEnd property is set.
+    /// </summary>
+    private static void OnAutoScrollToEndChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        // The property can only be attached to a TextBox.
+        if (d is not TextBox textBox)
+        {
+            return;
+        }
+
+        // The event handler that will perform the scroll
+        var scrollHandler = new TextChangedEventHandler(
+            (sender, args) =>
+            {
+                // The ScrollToEnd() method is built into the TextBox control.
+                textBox.ScrollToEnd();
+            });
+
+        if ((bool)e.NewValue)
+        {
+            // If AutoScrollToEnd is set to true, subscribe to the TextChanged event.
+            textBox.TextChanged += scrollHandler;
+            // Also, scroll to end immediately in case there's already text.
+            textBox.ScrollToEnd();
+        }
+        else
+        {
+            // If AutoScrollToEnd is set to false, unsubscribe to prevent memory leaks.
+            textBox.TextChanged -= scrollHandler;
+        }
+    }
+}
